c10f7a3d198b50db3df413dd19ccdc29a63bb72e
Gillian Hammett <gillerbiller@gmail.com@hackbrightacademy.com>
2020-08-21 10:49:37 -0700

    adding more unittests

--- tests.py
+++ tests.py
@@ -3,30 +3,43 @@ import BST1
 
 class Test_binary_search_tree(unittest.TestCase):
 
-    def test_number_one(self):
+    def test_add_node(self):
 
         root = binary_search_tree(15)
-        root.left = binary_search_tree(10)
-        root.left.left = binary_search_tree(5)
-        root.left.right = binary_search_tree(14)
-        root.left.left.left = binary_search_tree(1)
-        root.left.left.right = binary_search_tree(5)
-        root.left.right.left = binary_search_tree(12)
-        root.right = binary_search_tree(20)
-        root.right.left = binary_search_tree(16)
-        root.right.right = binary_search_tree(24)
-        root.right.left.left = binary_search_tree(16)
-        root.right.left.right = binary_search_tree(18)
-        root.right.right.left = binary_search_tree(23)
-        root.right.right.right = binary_search_tree(35)
+        root.add_node(10)
+        root.add_node(5)
+        root.add_node(1)
+        root.add_node(14)
+        root.add_node(12)
+        root.add_node(20)
+        root.add_node(16)
+        root.add_node(16)
+        root.add_node(18)
+        root.add_node(23)
+        root.add_node(24)
+        root.add_node(35)
 
         root.add_node(14)
         self.assertTrue(root.left.right.right.value == 14)
 
-        root.delete_node
-
 
     def test_delete_node(self):
+        root = binary_search_tree(15)
+        root.add_node(10)
+        root.add_node(5)
+        root.add_node(1)
+        root.add_node(14)
+        root.add_node(12)
+        root.add_node(20)
+        root.add_node(16)
+        root.add_node(16)
+        root.add_node(18)
+        root.add_node(23)
+        root.add_node(24)
+        root.add_node(35)
+
+        root.test_delete_node(1)
+        self.assertTrue(root.left.left.left.value == None)
 
     def test_get_min_value(self):
 
